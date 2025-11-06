dd169bddc0149adad627ddb23f250139f2d6019b
Brendan van de Venter <Brendan1vandeventer@gmail.com>
2025-11-06 22:41:37 +0100

    completed vector outline

--- lsh.py
+++ lsh.py
@@ -2,9 +2,45 @@ import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
+all_g = [
+            'science fiction & fantasy',
+            'drama',
+            'western',
+            'comedy',
+            'classics',
+            'action & adventure',
+            'kids & family',
+            'musical & performing arts',
+            'documentary',
+            'art house & international',
+            'horror',
+            'sports & fitness',
+            'faith & spirituality',
+            'mystery & suspense',
+            'animation',
+            'special interest',
+            'romance'
+            ]
+
 def get_dataset():
     final_dataset = pd.read_csv('final_dataset.csv')
+
+    # Get all unique genres from your dataset
+    # all_genre_strings = final_dataset['genres'].dropna().unique()
+    # # Example: ["Action, Sci-Fi", "Drama", "Comedy, Romance", ...]
+
+    # # Parse and collect unique genres
+    # unique_genres = set()
+    # for genre_string in all_genre_strings:
+    #     genres = genre_string.split(', ')
+    #     unique_genres.update(genres)
+    # print(unique_genres)
+
     return final_dataset
+
+def encode_genres(movie_genres:list,all_genres:list):
+    return [1 if genre in movie_genres else 0 for genre in all_genres]
+
 
 def group():
 
@@ -45,7 +81,7 @@ def group():
         if len(movie['reviews']) >= min_reviews
     ]
 
-
+    #check min movie reviews
     # for movie in movie_data_filtered:
     #     num = len(movie['reviews'])
     #     if len(movie['reviews']) < num:
@@ -57,22 +93,59 @@ def group():
 
 def vectorize(movie_data_filtered: list):
 
+
     model = SentenceTransformer('all-MiniLM-L6-v2')
 
+    #dictionary of each movie 
     for movie in movie_data_filtered:
+
+        ################
+        #encode reviews:
+        ################
+
         # Encode all reviews for this movie
         review_embeddings = model.encode(movie['reviews'])# Shape: (num_reviews, 384)
 
-
         #Average to get single movie embedding
         movie['review_embeddings'] = review_embeddings.mean(axis=0)
-        # print(movie['review_embeddings']) 
+
+
+        ########################
+        #vectorize/norm metadata:
+        ########################
+
+        #Tomatometer
+        movie['tomatometer_norm'] = movie['tomatometer']/100
+
+        #Genres
+        all_genres = all_g
+        movie['genre_vector']= encode_genres(movie['genres'], all_genres)
+
+        ##############
+        #Final vector:
+        ##############
+        
+        final_vector = np.concatenate([
+            np.array(movie['review_embeddings']),
+            np.array([movie['tomatometer_norm']]),
+            np.array(movie['genre_vector'])
+        ])
+
+    print(final_vector)
+    print(movie['tomatometer_norm'])
+
+
+    # print(movie_data_filtered[0])
+
+
+
     
 
-
 def main():
-    group()
-    vectorize(group())
+    
+    grouped = group()
+
+    vector = vectorize(grouped)
 
 if __name__ == "__main__":
     main()

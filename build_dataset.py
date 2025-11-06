bd71e2f0a64668cf6f6b67fb7a07054d34d2b28a
Brendan van de Venter <s204770@student.dtu.dk>
2025-11-06 20:17:14 +0100

    final_dataset and started on lsh

--- build_dataset.py
+++ build_dataset.py
@@ -17,13 +17,42 @@ def merge_datasets():
 #chooses columns we want and filters out anything we dont want
 def fix_filter_dataset(df: pd.DataFrame):
 
-    df_fixed = df.dropna(subset=['review_content'])
-    print(df_fixed.info())
-    return df_fixed
+    
+    df_fixed = df.dropna(subset=['review_content','original_release_date'])#remove empty reviews
+
+    unwanted_columns = [
+        'critic_name',
+        'top_critic',
+        'publisher_name',
+        'movie_info',
+        'critics_consensus',
+        'content_rating',
+    ]
+    
+    #get rid of unwanted columns
+    df_filtered = df_fixed.drop(unwanted_columns, axis='columns')
+
+    # df_filtered['review_content'] = df_filtered['review_content'].str.lower()
+    df_filtered = df_filtered.apply(lambda x: x.str.lower() if x.dtype == "object" else x)
+
+    # print(df_fixed.info())
+    # print(df_filtered.info())
+
+    return df_filtered
+
+def create_csv(df: pd.DataFrame):
+
+    df[0:100000].to_csv('final_dataset.csv')
+
+
 
 def main():
-    df_merged = merge_datasets()
-    fix_filter_dataset(df_merged)
+
+    # df_merged = merge_datasets()
+    # df_ff = fix_filter_dataset(df_merged)
+    # create_csv(df_ff)
+
+    create_csv(fix_filter_dataset(merge_datasets()))
 
 if __name__ == "__main__":
     main()
---
+++ lsh.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+def get_dataset():
+    final_dataset = pd.read_csv('final_dataset.csv')
+    return final_dataset
+
+def group():
+
+    final_dataset = get_dataset()
+
+    grouped = final_dataset.groupby('rotten_tomatoes_link')
+
+    # print(grouped['movie_title'].first())
+    
+    movie_data = []
+
+    for movie_id, movie_reviews in grouped:
+
+        reviews = movie_reviews['review_content'].tolist()
+
+        # Get metadata (take first row since they're all the same)
+        first_row = movie_reviews.iloc[0]
+
+        movie_data.append({
+            'movie_id': movie_id,
+            'movie_title': first_row['movie_title'],
+            'reviews': reviews,  # List of all review texts
+            'runtime': first_row['runtime'],
+            'year': int((first_row['original_release_date']).split('-')[0]),
+            'tomatometer': first_row['tomatometer_rating'],
+            'genres': first_row['genres']
+        })
+
+    
+    # print(len(movie_data))
+    # print(movie_data[85]['year'])
+
+    #keep movies above minimum amount of reviews
+    min_reviews = 5
+
+    movie_data_filtered = [
+        movie for movie in movie_data
+        if len(movie['reviews']) >= min_reviews
+    ]
+
+
+    # for movie in movie_data_filtered:
+    #     num = len(movie['reviews'])
+    #     if len(movie['reviews']) < num:
+    #         num = len(movie['reviews'])
+
+    # print(num)
+
+    return movie_data_filtered
+
+def vectorize(movie_data_filtered: list):
+
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    for movie in movie_data_filtered:
+        
+     review_embeddings = model.encode(movie['reviews'])
+
+    print(review_embeddings)
+
+
+def main():
+    group()
+    vectorize(group())
+
+if __name__ == "__main__":
+    main()

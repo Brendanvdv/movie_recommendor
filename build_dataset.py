95bd80aee58eb0d0c77daf4d02e7d5b722af670b
Brendan van de Venter <s204770@student.dtu.dk>
2025-11-11 18:28:56 +0100

    finalized dataset builder

--- build_dataset.py
+++ build_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from typing import cast
 
 df_critic_reviews = pd.read_csv('rotten_tomatoes_critic_reviews.csv')
 df_movies = pd.read_csv('rotten_tomatoes_movies.csv')
@@ -10,15 +11,12 @@ def merge_datasets():
     df_merged = pd.merge(df_critic_reviews,df_movies,on='rotten_tomatoes_link', how='inner')
     return df_merged
     
-    
-    
 #chooses columns we want and filters out anything we dont want
-def fix_filter_dataset(df: pd.DataFrame):
+def fix_filter_dataset(df: pd.DataFrame) -> pd.DataFrame:
 
     #remove empty rows
     df_fixed = df.dropna(subset=['review_content','original_release_date','genres','runtime'])
     
-
     unwanted_columns = [
         'critic_name',
         'top_critic',
@@ -31,28 +29,25 @@ def fix_filter_dataset(df: pd.DataFrame):
     #get rid of unwanted columns
     df_filtered = df_fixed.drop(unwanted_columns, axis='columns')
 
-    #only fresh(positive) reviews
-    df_filtered = df_filtered[df_filtered['review_type'] == 'fresh']
-
     #lowercase entire dataframe
-    df_filtered = df_filtered.apply(lambda x: x.str.lower() if x.dtype == "object" else x)
+    # df_filtered = df_filtered.applymap(lambda x: x.str.lower() if x.dtype == "object" else x)
 
-    
+    for col in df_filtered.select_dtypes(include=['object']).columns:
+        df_filtered[col] = df_filtered[col].str.lower()
+
+    # only keep fresh reviews
+    # df_filtered = cast(pd.DataFrame, df_filtered[df_filtered['review_type'] == 'fresh'])
 
     return df_filtered
 
 def create_csv(df: pd.DataFrame):
 
-    df[0:1000].to_csv('final_dataset.csv')
-    # df.to_csv('final_dataset.csv')
+    # df[0:1000].to_csv('final_dataset.csv')
+    df.to_csv('final_dataset.csv')
 
 
 
 def main():
-
-    # df_merged = merge_datasets()
-    # df_ff = fix_filter_dataset(df_merged)
-    # create_csv(df_ff)
 
     create_csv(fix_filter_dataset(merge_datasets()))
 

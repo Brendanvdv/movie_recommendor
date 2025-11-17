7341207401f99042d02046fd27611c9205ff980f
Brendan van de Venter <Brendan1vandeventer@gmail.com>
2025-11-17 08:36:31 +0100

    final build with review type

--- similarity.py
+++ similarity.py
@@ -24,7 +24,7 @@ all_age_ratings = ['pg', 'r', 'g', 'pg-13', 'nc17']
 
 #Movie Data-------------------------------------------------------------------------------------------
 
-def get_dataset():
+def get_dataset() -> pd.DataFrame:
 
     dataset = 'final_dataset.csv'
 
@@ -36,12 +36,10 @@ def group() -> list[dict]:
 
     final_dataset = get_dataset()
     
-
     #group movies together
     grouped = final_dataset.groupby('rotten_tomatoes_link')
 
     movie_data = []
-
 
     for movie_id, group in grouped:
 
@@ -49,6 +47,7 @@ def group() -> list[dict]:
         reviews = group['review_content'].tolist()
         review_scores = group['review_score'].tolist()
         critic_names = group['critic_name'].to_list()
+        review_types = group['review_type'].to_list()
 
         # Get metadata (take first row since they're all the same)
         first_row = group.iloc[0]
@@ -66,22 +65,22 @@ def group() -> list[dict]:
             'reviews': reviews,  # List of all review texts
             'review_scores': review_scores, #List of all review scores
             'critic_names': critic_names,   #List of all critic names
+            'review_types': review_types, #List of all review types
             
             #Info
+            # 'avg_review_type': np.mean(review_types), 
             'avg_score': np.mean(review_scores),
             'num_reviews': len(reviews)
 
         })
-    
-
-    #keep movies above minimum amount of reviews
+        
+    #minimum amount of reviews
     min_reviews = 5
 
     movie_data_filtered = [
         movie for movie in movie_data
         if len(movie['reviews']) >= min_reviews
     ]
-    
     
     print(f"Loaded {len(movie_data_filtered)} movies with ≥{min_reviews} reviews")
     movie_data = movie_data_filtered
@@ -101,7 +100,10 @@ def vectorize(movie_data: list) -> list[dict]:
     #creates binary list of ratings
     def encode_content_rating(content_rating,all_content_ratings):
         return [1 if cr == content_rating else 0 for cr in all_content_ratings]
-
+    
+    def encode_review_type(review_types):
+        return [1 if rt == "fresh" else 0 for rt in review_types]
+       
     print("Initializing sentence transformer...")
     model = SentenceTransformer('paraphrase-MiniLM-L3-v2')#faster but worse perfomance
     # model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -111,13 +113,10 @@ def vectorize(movie_data: list) -> list[dict]:
     min_year = min(year)
     max_year = max(year)
 
-
     total_movies = len(movie_data)
 
-    
     for idx, movie in enumerate(movie_data, start=1):
 
-    
         # Print progress
         print(f"Processing movie {idx}/{total_movies} ({idx/total_movies*100:.1f}%) - {movie['movie_title']}")
 
@@ -173,13 +172,19 @@ def vectorize(movie_data: list) -> list[dict]:
         #Review rating
         movie['avg_score_norm']= movie['avg_score']/100
 
+        ########
+        #Review Type
+        ########
+
+        movie['review_types_norm'] = encode_review_type(movie['review_types'])
+       
     vectorized_movie_data = movie_data
 
-    # print(vectorized_movie_data[0])
+    print(vectorized_movie_data[0])
     return vectorized_movie_data
 
 #checks Load vectorized movie data from pickle if it exists. Otherwise, generate, save, and return it
-def load_or_create_vectorized_data(pickle_path="vectorized_movie_data.pkl"):
+def load_or_create_vectorized_data(pickle_path="vectorized_movie_data.pkl") -> list[dict]:
     if os.path.exists(pickle_path):
         print(f"Loading vectorized movie data from {pickle_path}...")
         with open(pickle_path, "rb") as f:
@@ -197,7 +202,7 @@ def load_or_create_vectorized_data(pickle_path="vectorized_movie_data.pkl"):
 #Similarity-----------------------------------------------------------------------------------------
 
 #content
-def build_content_sim(vectorized_movie_data):
+def build_content_sim(vectorized_movie_data) -> np.ndarray:
 
     content_vectors = np.vstack([movie['content_vector'] for movie in vectorized_movie_data])
 
@@ -210,7 +215,7 @@ def build_content_sim(vectorized_movie_data):
     return content_sim
     
 #style
-def build_review_style_sim(vectorized_movie_data):
+def build_review_style_sim(vectorized_movie_data) -> np.ndarray:
     
     review_embeddings = np.vstack([movie['avg_review_embeddings'] for movie in vectorized_movie_data])
 
@@ -241,10 +246,10 @@ def build_review_score_sim(vectorized_movie_data):
     #Fill in review scores
     for movie in vectorized_movie_data:
 
-        mit = movie['movie_title']
+        mt = movie['movie_title']
 
         for critic,score in zip(movie['critic_names'], movie['review_scores']):
-            matrix.loc[mit,critic] = score
+            matrix.loc[mt,critic] = score
 
     #Pearson correlation movie x movie (1 to -1)
     score_sim = matrix.T.corr(method='pearson')
@@ -262,6 +267,46 @@ def build_review_score_sim(vectorized_movie_data):
 
     return score_sim
 
+
+def build_review_type_sim(vectorized_movie_data) -> np.ndarray:
+
+    #Get all critics
+    all_critics = set()
+    for movie in vectorized_movie_data:
+        all_critics.update(movie['critic_names'])
+    all_critics = sorted(all_critics)
+
+    #Get all movie titles
+    all_movies = [movie['movie_title'] for movie in vectorized_movie_data]
+    
+    #create Movie x Critic matrix
+    matrix = pd.DataFrame(np.nan, index = all_movies, columns=all_critics)
+
+    #Fill in review scores
+    for movie in vectorized_movie_data:
+
+        mit = movie['movie_title']
+
+        for critic,review_type in zip(movie['critic_names'], movie['review_types_norm']):
+            matrix.loc[mit,critic] = review_type
+
+    #Pearson correlation movie x movie (1 to -1)
+    type_sim = matrix.T.corr(method='pearson')
+
+    # normalize to 0-1
+    type_sim = (type_sim + 1) / 2
+
+    #convert to np array
+    type_sim_array = type_sim.values
+
+    #fill empty pairs with 0.5(neutral)
+    type_sim = np.nan_to_num(type_sim_array, nan=0.5)
+
+    print(f"Type similarity matrix: {type_sim.shape}")
+
+    print(type_sim)
+    return type_sim
+
 def load_or_create_similarity_matrices(vectorized_data, pickle_path="similarity_matrices.pkl"):
     """Load or compute all similarity matrices"""
     
@@ -271,33 +316,31 @@ def load_or_create_similarity_matrices(vectorized_data, pickle_path="similarity_
             matrices = pickle.load(f)
         content_sim = matrices['content']
         style_sim = matrices['style']
-        score_sim = matrices['score']
+        type_sim = matrices['type']
     else:
         print("Pickle file not found. Computing similarity matrices...")
         content_sim = build_content_sim(vectorized_data)
         style_sim = build_review_style_sim(vectorized_data)
-        score_sim = build_review_score_sim(vectorized_data)
+        type_sim = build_review_type_sim(vectorized_data)
         
         print(f"Saving similarity matrices to {pickle_path}...")
         with open(pickle_path, "wb") as f:
             pickle.dump({
                 'content': content_sim,
                 'style': style_sim,
-                'score': score_sim
+                'type': type_sim
             }, f)
     
-    return content_sim, style_sim, score_sim
+    return content_sim, style_sim, type_sim
 #Query--------------------------------------------------------------------------------------------------
 
-def hybrid_score(content_sim,review_sim,score_sim,alpha=0.4, beta=0.3, gamma=0.3):
+def hybrid_score(content_sim,review_sim,type_sim,alpha, beta, gamma):
     
-    hybrid_sim = (alpha * content_sim) + (beta * review_sim) + (gamma * score_sim)
+    hybrid_sim = (alpha * content_sim) + (beta * review_sim) + (gamma * type_sim)
 
     return hybrid_sim
 
-
 def query_movie(movie_title,vectorized_movie_data,hybrid_sim,k=10):
-
 
     #Find iterable of query movie
     query_id = None
@@ -312,32 +355,32 @@ def query_movie(movie_title,vectorized_movie_data,hybrid_sim,k=10):
 
     recommendations = []
 
-
     for pos in sorted_indices[1:k+1]:
         recommendations.append((vectorized_movie_data[pos]['movie_title'], sim[pos]))
 
+    ################################
+    max_len = max(len(movie) for movie, _ in recommendations)
     print()
-    print(f"Query movie: {movie_title} ")
+    print(f"Query movie: {movie_title}")
     print()
-    for m,s in recommendations:
-        print(f'{m}, {round(s,2)}')
+    print("MOVIE".ljust(max_len)  +  "  SCORE")
 
+    for m,s in recommendations:
+        score = round(s,2)
+        print(f'{m:{max_len}}  {score}')
+       
     return recommendations
 
 #----------------------------------------------------------------------------------------------------------
 
 def main():
 
-  
     vectorized_data = load_or_create_vectorized_data()
 
-    # overlaps = analyze_critic_overlap(vectorized_data)
-  
-    content_sim, style_sim, score_sim = load_or_create_similarity_matrices(vectorized_data)
-    hybrid_sim = hybrid_score(content_sim, style_sim, score_sim, alpha=0.5, beta=0.4, gamma=0.1)
+    content_sim, style_sim, type_sim = load_or_create_similarity_matrices(vectorized_data)
+    hybrid_sim = hybrid_score(content_sim, style_sim, type_sim, alpha=0.5, beta=0.4, gamma=0.1)
     
-
-    query_title = "titanic"
+    query_title = "the shining"
 
     query_movie(query_title, vectorized_data, hybrid_sim, k=10)
 
